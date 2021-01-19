6c1344f734d7f3e550fcf06d0e371b2edd80da9b
sakir tufan <sakirtufan49@gmail.com>
2021-01-19 20:01:37 +0100

    Modifie Product.jsx

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,55 +1,62 @@
 import React from "react";
 import { MdAddShoppingCart } from "react-icons/md";
-import { connect, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addToCart } from "../../redux/actions/cartActions";
-import { removeProductFromList } from '../../redux/actions/productActions'
+import { removeProductFromList } from "../../redux/actions/productActions";
 import alertify from "alertifyjs";
 import { BiEdit, BiTrash } from "react-icons/bi";
 import { Link, useHistory } from "react-router-dom";
 
-const Product = (props) => {
-
-  const dispatch = useDispatch();  
+const Product = ({ product }) => {
+  const dispatch = useDispatch();
   const history = useHistory();
 
-  const addToCart = (product) => {
-    props.addToCart({quantity:1,product})
-    alertify.notify(`${product.productName} added to cart`)
-  }
+  const addProductToCart = (product) => {
+    dispatch(addToCart({ quantity: 1, product }));
+    alertify.notify(`${product.productName} added to cart`);
+  };
 
   const onHandleDelete = (product) => {
-    dispatch(removeProductFromList(product.id))
-    alertify.error(`${product.productName} removed from product list`)
-    history.push('/')
-  }
-
+    dispatch(removeProductFromList(product.id));
+    alertify.error(`${product.productName} removed from product list`);
+    history.push("/");
+  };
 
   return (
     <tr>
-      <th scope="row">{props.product.id}</th>
-      <td>{props.product.productName}</td>
-      <td>{props.product.unitPrice}</td>
-      <td>{props.product.quantityPerUnit}</td>
-      <td>{props.product.unitsInStock}</td>
+      <th scope="row">{product.id}</th>
+      <td>{product.productName}</td>
+      <td>{product.unitPrice}</td>
+      <td>{product.quantityPerUnit}</td>
+      <td>{product.unitsInStock}</td>
       <td>
-        <button onClick={()=>addToCart(props.product)} className="btn btn-outline-secondary">
+        <button
+          onClick={() => addProductToCart(product)}
+          className="btn btn-outline-secondary"
+        >
           <MdAddShoppingCart />
         </button>
       </td>
       <td>
-        <Link to={`/products/${props.product.id} `} className="btn btn-outline-primary"><BiEdit/></Link>
+        <Link
+          to={`/products/${product.id} `}
+          className="btn btn-outline-primary"
+        >
+          <BiEdit />
+        </Link>
       </td>
       <td>
-        <button onClick={() => {onHandleDelete(props.product)}} className="btn btn-outline-danger"><BiTrash/></button>
+        <button
+          onClick={() => {
+            onHandleDelete(product);
+          }}
+          className="btn btn-outline-danger"
+        >
+          <BiTrash />
+        </button>
       </td>
     </tr>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    cart: state.cartReducer.cart,
-  };
-};
-
-export default connect(mapStateToProps, { addToCart})(Product);
+export default Product;
