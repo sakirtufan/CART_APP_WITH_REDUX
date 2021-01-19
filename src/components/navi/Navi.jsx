8405a86ed39849aa26af825641fbf28c0abb4f73
sakir tufan <sakirtufan49@gmail.com>
2021-01-19 21:12:42 +0100

    Update Navi

--- src/components/navi/Navi.jsx
+++ src/components/navi/Navi.jsx
@@ -7,7 +7,6 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  Button,
 } from "reactstrap";
 import CartSummary from "../cart/CartSummary";
 
@@ -17,22 +16,26 @@ const Navi = (props) => {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-  
-      <Navbar color="light" light expand="md" className="p-3 mb-5">
-        <NavbarBrand href="/"><div className="p-1 mb-2 bg-success text-light font-weight-bold rounded">HOME</div>  </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem className='pr-3'>
-              <Link to="/add">
-              <div className="p-2 mb-1 bg-warning text-dark font-weight-bold rounded">Add Product</div>             
-              </Link>
-            </NavItem>
-            <CartSummary />
-          </Nav>
-        </Collapse>
-      </Navbar>
-    
+    <Navbar color="light" light expand="md" className="p-3 mb-5">
+      <NavbarBrand href="/">
+        <div className="p-1 mb-2 bg-success text-light font-weight-bold rounded">
+          HOME
+        </div>{" "}
+      </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem className="pr-3">
+            <Link to="/add">
+              <div className="p-2 mb-1 bg-warning text-dark font-weight-bold rounded">
+                Add Product
+              </div>
+            </Link>
+          </NavItem>
+          <CartSummary />
+        </Nav>
+      </Collapse>
+    </Navbar>
   );
 };
 
